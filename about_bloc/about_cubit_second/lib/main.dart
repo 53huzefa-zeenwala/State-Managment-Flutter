d20c8bc3b6f6e5c2163b63daa61301413a343a59
huzefa zeenwala <huzefa.zeenwala53@gmail.com>
2023-11-02 15:44:54 +0530

    initial about_bloc_second

---
+++ about_bloc/about_cubit_second/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:bl';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("home Page"),
+      ),
+    );
+  }
+}
---
+++ w
@@ -0,0 +1,23 @@
+[33mcommit 3c0a582ef10111fcae3cf80ee248ec8c1cab8119[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: huzefa zeenwala <huzefa.zeenwala53@gmail.com>
+Date:   Thu Nov 2 11:52:46 2023 +0530
+
+    Completed about-bloc_first with block text
+
+[33mcommit f64f36f4c7301631cdc40115c196b7e3391ad225[m
+Author: huzefa zeenwala <huzefa.zeenwala53@gmail.com>
+Date:   Wed Nov 1 14:48:07 2023 +0530
+
+    Starting with bloc / Cubit
+
+[33mcommit 67beabee9989b125e6de5c21ccb214934aa3ec04[m
+Author: huzefa zeenwala <huzefa.zeenwala53@gmail.com>
+Date:   Wed Nov 1 13:51:24 2023 +0530
+
+    Completed Redux
+
+[33mcommit fc8a8e65ec034eba286e992149c0657ebcf7042e[m
+Author: huzefa zeenwala <huzefa.zeenwala53@gmail.com>
+Date:   Wed Nov 1 12:40:42 2023 +0530
+
+    first commit
